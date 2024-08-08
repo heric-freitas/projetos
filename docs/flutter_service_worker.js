fedd3e2afd26898450adfd1211f0902c7ccac1fa
heric-freitas <110538972+heric-freitas@users.noreply.github.com>
2024-08-08 11:36:11 -0300

    nova tentativa

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "fb53dc138b492c8306470a6b4ab4da9d
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
-"flutter_bootstrap.js": "594dab42fadab2d8bdc021f60160b971",
+"flutter_bootstrap.js": "95c78f886692c860333a0ffbf91a346b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
